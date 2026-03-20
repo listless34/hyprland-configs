b651a0a1ffbfb3d9ebe86079c275cc883ad8da00
Listless34 <listless34@gmail.com>
2026-03-20 15:08:57 +0530

    aliases file added

---
+++ aliases.zsh
@@ -0,0 +1,22 @@
+# file to put aliases of zsh
+#
+
+alias ls="exa -al"
+alias y="yay -S"
+alias ys="yay -Ss"
+alias yc="yay -Scc"
+alias yr="yay -Rns"
+alias update="yay -Syu"
+alias begin="start-hyprland"
+alias p="sudo pacman -S --needed"
+alias ps="pacman -Ss"
+alias tls="tmux ls"
+alias tm="tmux"
+alias ta="tmux attach"
+alias cr="cargo run"
+alias ct="cargo test"
+alias cat="bat"
+alias fp='flatpak --installation=extra'
+alias ga="git add ."
+alias gi="git init"
+alias gs="git status"
